7f3138fd3290d7ad37232febe113e9f3c20a1b70
yuktiguru1110 <Yukti.Guru@lntinfotech.com>
2020-09-09 17:41:09 +0530

    sum

---
+++ add/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a == 0)
+            {
+                Console.WriteLine("Write Again");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            if (b == 0)
+            {
+                Console.WriteLine("Write Again");
+                b = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = a + b;
+            int product = a * b;
+            Console.WriteLine("sum : {0}, product : {1}", sum, product);
+        }
+    }
+}
